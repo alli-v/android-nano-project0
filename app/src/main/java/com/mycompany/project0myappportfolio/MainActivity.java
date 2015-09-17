3af7623beb54eb3340e5f8a7f3da76170d992d9d
Alli Shepherd <allishepherd10@gmail.com>
2015-09-17 11:32:33 -0400

    updates based on TA feedback

--- app/src/main/java/com/mycompany/project0myappportfolio/MainActivity.java
+++ app/src/main/java/com/mycompany/project0myappportfolio/MainActivity.java
@@ -20,21 +20,14 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toast mAppToast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     @Override
@@ -44,20 +37,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     //display toast message for each button item on click
     public void sendMessage(View view) {
@@ -66,7 +45,12 @@ public class MainActivity extends AppCompatActivity {
         String btnText = "This button will launch my " + btn.getText().toString() + " project!";
         int duration = Toast.LENGTH_SHORT;
 
-        Toast toast = Toast.makeText(context, btnText, duration);
-        toast.show();
+        //stop previous toast to prevent delay
+        if(mAppToast != null) {
+            mAppToast.cancel();
+        }
+
+        mAppToast = Toast.makeText(context, btnText, duration);
+        mAppToast.show();
     }
 }
